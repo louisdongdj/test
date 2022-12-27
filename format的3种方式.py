066bb037467df92d05a2396096fc520fb90d2a57
louisdongdj <121493307+louisdongdj@users.noreply.github.com>
2022-12-27 19:39:35 +0800

    Add files via upload

---
+++ format的3种方式.py
@@ -0,0 +1,13 @@
+def getname1(name):
+    print(f'我叫{name}')
+
+
+def getname2(name):
+    print('我叫%s' %name)
+
+def getname3(name):
+    print('我叫{}'.format(name))
+
+getname1('djh')
+getname2('djh')
+getname3('djh')
